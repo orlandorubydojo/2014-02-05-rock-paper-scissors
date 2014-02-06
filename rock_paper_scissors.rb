58427f2927867a0d14fb6bdfbb0813045b339610
Trace Helms <tracehelms@gmail.com>
2014-02-05 20:37:08 -0500

    Progress from dojo

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,2 +1,23 @@
 class RockPaperScissors
+
+  def randomHand
+    hands = ["rock", "paper", "scissors"]
+    hands.sample
+  end
+
+  def compareHands(hand1, hand2)
+    wins_against = {
+      "rock" => "scissors",
+      "scissors" => "paper",
+      "paper" => "rock"
+    }
+    if hand1 == hand2
+      return "tie"
+    elsif wins_against[hand1] == hand2
+      return hand1
+    else
+      return hand2
+    end
+  end
+
 end
--- rock_paper_scissors_spec.rb
+++ rock_paper_scissors_spec.rb
@@ -2,7 +2,27 @@ require './rock_paper_scissors.rb'
 
 describe RockPaperScissors do
 
-  it 'first test' do
+  before(:each) do
+    @rpc = RockPaperScissors.new
   end
+
+  it 'picks a random hand' do
+    expect(["rock", "paper", "scissors"]).to include(@rpc.randomHand)
+  end
+
+  it 'sees if rock loses to paper' do
+    expect(@rpc.compareHands("rock", "paper")).to eq("paper")
+  end
+
+  it 'sees if rock beats scissors' do
+    expect(@rpc.compareHands("rock", "scissors")).to eq("rock")
+  end
+
+  it 'sees if two of the same hands tie' do
+    expect(@rpc.compareHands("rock", "rock")).to eq("tie")
+    expect(@rpc.compareHands("paper", "paper")).to eq("tie")
+    expect(@rpc.compareHands("scissors", "scissors")).to eq("tie")
+  end
+
 
 end
